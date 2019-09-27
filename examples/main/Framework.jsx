9c9c94265867924aa84b502e30237d2eddf95ce8
陈希 <995101@nd.com>
2019-09-27 13:56:21 +0800

    主框架加入antd，TODO：解决子应用样式冲突

--- examples/main/Framework.jsx
+++ examples/main/Framework.jsx
@@ -3,12 +3,15 @@
  * @since 2019-05-16
  */
 
-import React from 'react';
+import React, { useState } from 'react';
+import { Spin, Menu } from 'antd'
 import style from './index.less';
 
 export default function Framework(props) {
 
   const { content, loading } = props;
+
+  const [ currentMenuItem, switchMenuItem ] = useState('1')
 
   function goto(title, href) {
     window.history.pushState({}, title, href);
@@ -22,22 +25,16 @@ export default function Framework(props) {
 
   return (
     <>
-      <header className={style.header}>
-        <nav>
-          <ol>
-            <li><a onClick={() => goto('react app', '/react')} title='react16 + antd3'>子应用1</a></li>
-            <li><a onClick={() => goto('react15 app', '/15react15')} title='react15 + antd2'>子应用2</a></li>
-            <li><a onClick={() => goto('vue app', '/vue')} title='vue2 + element2'>子应用3</a></li>
-          </ol>
-        </nav>
-      </header>
-      {
-        // <button onClick={setInterval}>set master interval</button>
-      }
-      <div className={style.body}>
-        {loading ? <div className={style.loading}>loading...</div> : null}
-        <div dangerouslySetInnerHTML={{ __html: content }} className={style.appContainer}/>
-      </div>
+      <Menu defaultOpenKeys={['1']} onClick={(e) => {switchMenuItem(e.key); console.log(e)}} selectedKeys={[currentMenuItem]} mode='horizontal' theme='dark' className={style.header}>
+        <Menu.Item key={'1'} onClick={() => goto('react app', '/react')} title='react16 + antd3'>子应用1</Menu.Item>
+        <Menu.Item key={'2'} onClick={() => goto('react15 app', '/15react15')} title='react15 + antd2'>子应用2</Menu.Item>
+        <Menu.Item key={'3'} onClick={() => goto('vue app', '/vue')} title='vue2 + element2'>子应用3</Menu.Item>
+      </Menu>
+      <Spin spinning={loading} size='large'>
+        <div className={style.body}>
+          <div dangerouslySetInnerHTML={{ __html: content }} className={style.appContainer} />
+        </div>
+      </Spin>
     </>
 
   );
